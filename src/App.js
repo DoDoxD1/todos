332d1e97593d708e89343c9e6171551c1259ed9a
aunuj <aunujain53@gmail.com>
2023-09-25 02:11:59 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Header from './MyComponents/header';
 import Footer from './MyComponents/Footer';
 import Todo from './MyComponents/TodosList';
+import About from './MyComponents/About';
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   let initTodo;
@@ -35,11 +37,23 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
-      <Todo todos={todos} addTodo={addTodo} deleteTodo={deleteTodo} />
+
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Todo todos={todos} addTodo={addTodo} deleteTodo={deleteTodo} />
+          }
+        />
+
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/MyComponents/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function About() {
+  const myStyle = {
+    minHeight: '85vh',
+  };
+  return (
+    <>
+      <div style={myStyle} className="m-3">
+        <h3>About</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+          possimus laboriosam quasi reprehenderit amet dolor adipisci est.
+          Repellendus ab nobis incidunt eius expedita! Asperiores accusantium
+          amet eaque fuga tempora consequuntur officia minima iusto. Laudantium,
+          iste!
+        </p>
+      </div>
+    </>
+  );
+}
